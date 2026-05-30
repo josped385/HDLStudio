622b140b7f3e49288ab416e976c2a602ce3acc89
josped385 <193796023+josped385@users.noreply.github.com>
2026-05-30 17:34:50 +0200

    colored popup

--- widgets/code_editor.py
+++ widgets/code_editor.py
@@ -1,13 +1,145 @@
 import os
 from PyQt6.QtGui import QColor, QFont
 from PyQt6.Qsci import QsciScintilla, QsciScintillaBase
-from PyQt6.QtCore import pyqtSignal, QTimer, Qt
-from PyQt6.QtWidgets import QToolTip
+from PyQt6.QtCore import pyqtSignal, Qt, QPoint, QTimer
+from PyQt6.QtWidgets import QFrame, QLabel, QVBoxLayout
 
 from widgets.lexers import VerilogLexer, VHDLLexer
 
+_VERILOG_KW = frozenset({
+    "module", "endmodule", "input", "output", "inout", "wire", "reg", "logic",
+    "integer", "real", "time", "parameter", "localparam", "assign", "always",
+    "initial", "begin", "end", "if", "else", "case", "endcase", "casex",
+    "casez", "for", "while", "repeat", "forever", "function", "endfunction",
+    "task", "endtask", "generate", "endgenerate", "genvar", "posedge",
+    "negedge", "or", "and", "nand", "nor", "xor", "xnor", "not", "buf",
+    "bufif0", "bufif1", "notif0", "notif1", "supply0", "supply1", "tri",
+    "tri0", "tri1", "triand", "trior", "trireg", "uwire", "wand", "wor",
+    "always_comb", "always_ff", "always_latch", "typedef", "enum", "struct",
+    "union", "package", "endpackage", "interface", "endinterface",
+    "modport", "clocking", "endclocking", "program", "endprogram",
+    "assert", "assume", "cover", "property", "endproperty", "sequence",
+    "endsequence", "specify", "endspecify", "pulsestyle_onevent",
+    "pulsestyle_ondetect", "showcancelled", "signed", "unsigned",
+    "automatic", "fork", "join", "join_any", "join_none",
+    "disable", "wait", "event", "deassign", "force", "release",
+})
 
-_IDENTIFIER_RE = None
+_VHDL_KW = frozenset({
+    "entity", "is", "port", "in", "out", "inout", "end", "architecture",
+    "of", "begin", "signal", "variable", "constant", "type", "subtype",
+    "function", "procedure", "process", "if", "then", "else", "elsif",
+    "when", "others", "case", "loop", "for", "while", "generate",
+    "component", "map", "generic", "all", "use", "library", "work",
+    "ieee", "std", "rising_edge", "falling_edge", "to", "downto",
+    "range", "open", "bus", "register", "file", "attribute", "group",
+    "impure", "pure", "return", "null", "report", "severity", "note",
+    "warning", "error", "failure", "body", "protected", "unprotected",
+})
+
+_HDL_TYPES = frozenset({
+    "wire", "reg", "logic", "integer", "real", "time", "tri", "tri0",
+    "tri1", "triand", "trior", "trireg", "uwire", "wand", "wor",
+    "supply0", "supply1", "bit", "byte", "shortint", "int", "longint",
+    "shortreal", "string", "chandle", "void", "signed", "unsigned",
+    "std_logic", "std_logic_vector", "std_ulogic", "std_ulogic_vector",
+    "bit_vector", "boolean", "natural", "positive", "character",
+    "signed", "unsigned",
+})
+
+
+def _get_palette():
+    from themes.theme_manager import ThemeManager
+    colors = ThemeManager.colors()
+    text = colors.get("text", "#ffffff")
+    r = int(text[1:3], 16)
+    g = int(text[3:5], 16)
+    b = int(text[5:7], 16)
+    is_dark = (r + g + b) // 3 > 128
+    from widgets.lexers import _COLORS_DARK, _COLORS_LIGHT
+    return _COLORS_DARK if is_dark else _COLORS_LIGHT
+
+
+def _hover_to_html(text):
+    pal = _get_palette()
+    kw_color = pal["keyword"]
+    type_color = pal["type"]
+    num_color = pal["number"]
+    str_color = pal["string"]
+    url_color = "#6cb6ff"
+    comment_color = pal["comment"]
+
+    lines = text.split("\n")
+    out_lines = []
+    for line in lines:
+        stripped = line.strip()
+        if stripped.startswith("http://") or stripped.startswith("https://"):
+            out_lines.append(f'<span style="color:{url_color};">{_escape(line)}</span>')
+            continue
+        tokens = _tokenize_line(line)
+        parts = []
+        for kind, val in tokens:
+            if kind == "keyword":
+                parts.append(f'<span style="color:{kw_color};font-weight:bold;">{_escape(val)}</span>')
+            elif kind == "type":
+                parts.append(f'<span style="color:{type_color};">{_escape(val)}</span>')
+            elif kind == "number":
+                parts.append(f'<span style="color:{num_color};">{_escape(val)}</span>')
+            elif kind == "string":
+                parts.append(f'<span style="color:{str_color};">{_escape(val)}</span>')
+            elif kind == "comment":
+                parts.append(f'<span style="color:{comment_color};">{_escape(val)}</span>')
+            else:
+                parts.append(_escape(val))
+        out_lines.append("".join(parts))
+    return "<br>".join(out_lines)
+
+
+def _escape(s):
+    return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace('"', "&quot;")
+
+
+_TOKEN_RE = None
+
+
+def _tokenize_line(line):
+    global _TOKEN_RE
+    if _TOKEN_RE is None:
+        _TOKEN_RE = __import__("re").compile(
+            r'//[^\n]*|"[^"]*"|'
+            r"\b\d+(?:'[ bodh]\s*[0-9a-fxz_?]+)?\b|"
+            r"\b[0-9a-fxz_]*'[ bodh]\s*[0-9a-fxz_?]+\b|"
+            r"\b[a-zA-Z_`$][\w$]*\b|"
+            r"\S"
+        )
+    pos = 0
+    tokens = []
+    for m in _TOKEN_RE.finditer(line):
+        val = m.group(0)
+        start, end = m.start(), m.end()
+        if pos < start:
+            tokens.append(("text", line[pos:start]))
+        lower = val.lower()
+        if val.startswith("//"):
+            tokens.append(("comment", val))
+        elif val.startswith('"'):
+            tokens.append(("string", val))
+        elif val[0].isdigit() or (len(val) > 1 and val[0] in "xz?" and val[1] in "'bodh"):
+            tokens.append(("number", val))
+        elif val.startswith("`"):
+            tokens.append(("directive", val))
+        elif lower in _VERILOG_KW or lower in _VHDL_KW or lower in (
+            "and", "or", "nand", "nor", "xor", "xnor", "not",
+        ):
+            tokens.append(("keyword", val))
+        elif lower in _HDL_TYPES:
+            tokens.append(("type", val))
+        else:
+            tokens.append(("text", val))
+        pos = end
+    if pos < len(line):
+        tokens.append(("text", line[pos:]))
+    return tokens
 
 
 def _get_identifier(text, pos):
@@ -25,6 +157,75 @@ def _get_identifier(text, pos):
     return text[start:end]
 
 
+class _TooltipPopup(QFrame):
+
+    def __init__(self, parent=None):
+        super().__init__(
+            parent,
+            Qt.WindowType.ToolTip | Qt.WindowType.FramelessWindowHint,
+        )
+        self.setAttribute(Qt.WidgetAttribute.WA_ShowWithoutActivating)
+        self.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+
+        self._label = QLabel(self)
+        self._label.setWordWrap(False)
+        self._label.setTextFormat(Qt.TextFormat.RichText)
+
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(10, 8, 10, 8)
+        layout.setSpacing(0)
+        layout.addWidget(self._label)
+        self.setLayout(layout)
+
+        self._auto_hide = QTimer(self)
+        self._auto_hide.setSingleShot(True)
+        self._auto_hide.setInterval(8000)
+        self._auto_hide.timeout.connect(self.hide)
+
+    def show_tip(self, html, gpos, colors):
+        bg = colors.get("editor_bg", "#1e1e1e")
+        fg = colors.get("editor_text", "#dcdcdc")
+        border = colors.get("border", "#555555")
+        font_family = colors.get("font_family", "Consolas")
+        font_size = colors.get("font_size", "10pt")
+        self.setStyleSheet(f"""
+            QFrame {{
+                background-color: {bg};
+                border: 1px solid {border};
+                border-radius: 4px;
+            }}
+            QLabel {{
+                background-color: transparent;
+                color: {fg};
+                font-family: {font_family}, Consolas, monospace;
+                font-size: {font_size};
+            }}
+        """)
+        self._label.setText(html)
+        self.adjustSize()
+        screen = self.screen()
+        if screen:
+            sg = screen.availableGeometry()
+            w = self.width()
+            h = self.height()
+            x = gpos.x() + 12
+            y = gpos.y() + 12
+            if x + w > sg.right():
+                x = gpos.x() - w - 12
+            if y + h > sg.bottom():
+                y = gpos.y() - h - 12
+            self.move(x, y)
+        else:
+            self.move(gpos.x() + 12, gpos.y() + 12)
+        self.show()
+        self.raise_()
+        self._auto_hide.start()
+
+    def hide_tip(self):
+        self._auto_hide.stop()
+        self.hide()
+
+
 class CodeEditor(QsciScintilla):
 
     cursor_position_changed = pyqtSignal(int, int)
@@ -36,26 +237,98 @@ class CodeEditor(QsciScintilla):
         self._theme_applied = False
         self._hover_db = None
         self._hover_language = None
-        self._hover_timer = QTimer(self)
-        self._hover_timer.setSingleShot(True)
-        self._hover_timer.timeout.connect(self._show_hover_tip)
-        self._last_hover_word = None
-        self._last_hover_pos = None
+        self._cached_lines = []
+        self._cached_margin_w = 0
+        self._last_tip_word = None
+        self._tip_popup = None
 
         self._setup_editor()
         self._setup_autocompletion()
-        self._setup_hover()
+
+        self.setMouseTracking(True)
 
         self.cursorPositionChanged.connect(
             self._on_cursor_changed
         )
+        self.textChanged.connect(self._cache_doc_lines)
+
+    def setText(self, text):
+        super().setText(text)
+        self._cache_doc_lines()
+
+    def _cache_doc_lines(self):
+        self._cached_lines = self.text().split("\n")
+        self._cached_margin_w = self.marginWidth(0)
 
     def set_hover_database(self, db):
         self._hover_db = db
 
-    def _setup_hover(self):
-        self.setMouseTracking(True)
-        self.SendScintilla(QsciScintillaBase.SCI_SETMOUSEDWELLTIME, 400)
+    def _word_at_global(self, gx, gy):
+        fm = self.fontMetrics()
+        lx = self.mapFromGlobal(QPoint(gx, gy)).x()
+        ly = self.mapFromGlobal(QPoint(gx, gy)).y()
+        fh = fm.height()
+        cw = fm.horizontalAdvance("n")
+        margin_w = self._cached_margin_w
+        line = ly // fh
+        if line < 0 or line >= len(self._cached_lines):
+            return None
+        col = max(0, (lx - margin_w) // cw)
+        text_line = self._cached_lines[line]
+        if col >= len(text_line):
+            col = len(text_line) - 1
+        ident = _get_identifier(text_line, col)
+        if ident:
+            return ident
+        if col > 0:
+            ident = _get_identifier(text_line, col - 1)
+            if ident:
+                return ident
+        if col + 1 < len(text_line):
+            ident = _get_identifier(text_line, col + 1)
+            if ident:
+                return ident
+        return None
+
+    def _get_or_create_popup(self):
+        if self._tip_popup is None:
+            self._tip_popup = _TooltipPopup(self)
+        return self._tip_popup
+
+    def _show_popup(self, gpos, tip):
+        popup = self._get_or_create_popup()
+        from themes.theme_manager import ThemeManager
+        colors = ThemeManager.colors()
+        html = _hover_to_html(tip)
+        popup.show_tip(html, gpos, colors)
+
+    def _hide_popup(self):
+        if self._tip_popup is not None:
+            self._tip_popup.hide_tip()
+
+    def mouseMoveEvent(self, event):
+        super().mouseMoveEvent(event)
+        if not self._hover_db:
+            return
+        gpos = self.viewport().mapToGlobal(event.position().toPoint())
+        word = self._word_at_global(gpos.x(), gpos.y())
+        if not word:
+            self._last_tip_word = None
+            self._hide_popup()
+            return
+        if word == self._last_tip_word:
+            return
+        self._last_tip_word = word
+        tip = self._hover_db.lookup(word, language=self._hover_language)
+        if tip:
+            self._show_popup(gpos, tip)
+        else:
+            self._hide_popup()
+
+    def leaveEvent(self, event):
+        super().leaveEvent(event)
+        self._last_tip_word = None
+        self._hide_popup()
 
     def showEvent(self, event):
         super().showEvent(event)
@@ -166,46 +439,3 @@ class CodeEditor(QsciScintilla):
     def _on_cursor_changed(self, line, index):
 
         self.cursor_position_changed.emit(line + 1, index + 1)
-
-    def _word_at_screen_pos(self, x, y):
-        pos = self.SendScintilla(QsciScintillaBase.SCI_POSITIONFROMPOINT, x, y)
-        if pos < 0:
-            return None, -1
-        text = self.text()
-        word = _get_identifier(text, pos)
-        return word, pos
-
-    def _show_hover_tip(self):
-        if not self._hover_db or self._last_hover_word is None:
-            return
-        tip = self._hover_db.lookup(self._last_hover_word, language=self._hover_language)
-        if tip:
-            screen_pos = self.mapToGlobal(self._last_hover_pos)
-            QToolTip.showText(screen_pos, tip, self)
-        else:
-            QToolTip.hideText()
-
-    def mouseMoveEvent(self, event):
-        super().mouseMoveEvent(event)
-        if not self._hover_db:
-            return
-        word, pos = self._word_at_screen_pos(int(event.position().x()),
-                                              int(event.position().y()))
-        if word and word != self._last_hover_word:
-            self._last_hover_word = word
-            self._last_hover_pos = event.position().toPoint()
-            if self._hover_db.lookup(word, language=self._hover_language):
-                self._hover_timer.start(400)
-            else:
-                self._hover_timer.stop()
-                QToolTip.hideText()
-        elif not word:
-            self._hover_timer.stop()
-            self._last_hover_word = None
-            QToolTip.hideText()
-
-    def leaveEvent(self, event):
-        super().leaveEvent(event)
-        self._hover_timer.stop()
-        self._last_hover_word = None
-        QToolTip.hideText()
